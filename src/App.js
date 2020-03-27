896c74113598141c6845db1970f1cf8b9b7bace9
JacobiClark <jacobcs@zoho.com>
2020-03-27 13:01:41 -0700

    netlify deploy fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
-import Comics from './components/Comics';
+import './style.css';
+import styled from 'styled-components'
+import Gallery from './components/Gallery';
 import Comic1 from './assets/comics/Comic1.PNG'
 import Comic2 from './assets/comics/Comic2.JPG'
 import Comic3 from './assets/comics/Comic3.PNG'
@@ -57,356 +58,122 @@ import NightDrive19 from './assets/storyboards/nightDrive/NightDrive19.JPG'
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router,
   Route, } from 'react-router-dom'
-import InkIllustrations from './components/InkIllustrations';
 import StoryboardMenu from './components/StoryboardMenu';
 import StoryboardCarousel from './components/StoryboardCarousel';
 import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const AppContainer = styled.div`
+  background-color: #292b2c;
+  background-size: contain;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  justify-content: center;
+  max-width: 2000px;
+`
+
 
 class App extends React.Component {
   state = {
     comics : [
-      {
-          id: 1,
-          title: 'Comic 1',
-          imgSrc: Comic1,
-      },
-      {
-          id: 2,
-          title: 'Comic 2 ',
-          imgSrc: Comic2,
-      },
-      {
-          id: 3, 
-          title: 'Comic 3',
-          imgSrc: Comic3,
-      },
-      {
-          id: 4,
-          title: 'Comic 4',
-          imgSrc: Comic4,
-      },
-      {
-          id: 5,
-          title: 'Comic 5',
-          imgSrc: Comic5,
-      },
-      {
-          id: 6,
-          title: 'Comic 6',
-          imgSrc: Comic6,
-      },
-      {
-          id: 7,
-          title: 'Comic 7',
-          imgSrc: Comic7,
-      },
-      {
-          id:8,
-          title: 'Comic 8',
-          imgSrc: Comic8,
-      },
-      {
-          id: 9,
-          title: 'Comic 9',
-          imgSrc: Comic9,
-      },
+      { id: 1, title: 'Comic 1', imgSrc: Comic1, },
+      { id: 2, title: 'Comic 2', imgSrc: Comic2, },
+      { id: 3, title: 'Comic 3', imgSrc: Comic3, },
+      { id: 4, title: 'Comic 4', imgSrc: Comic4, },
+      { id: 5, title: 'Comic 5', imgSrc: Comic5, },
+      { id: 6, title: 'Comic 6', imgSrc: Comic6, },
+      { id: 7, title: 'Comic 7', imgSrc: Comic7, },
+      { id: 8, title: 'Comic 8', imgSrc: Comic8, },
+      { id: 9, title: 'Comic 9', imgSrc: Comic9, },
     ],
     inkIllustrations : [
-      {
-          id: 1,
-          title: 'Illustration 1',
-          imgSrc: Illustration1,
-      },
-      {
-          id: 2,
-          title: 'Illustration 2 ',
-          imgSrc: Illustration2,
-      },
-      {
-          id: 3, 
-          title: 'Illustration 3',
-          imgSrc: Illustration3,
-      },
-      {
-          id: 4,
-          title: 'Illustration 4',
-          imgSrc: Illustration4,
-      },
-      {
-          id: 5,
-          title: 'Illustration 5',
-          imgSrc: Illustration5,
-      },
-      {
-          id: 6,
-          title: 'Illustration 6',
-          imgSrc: Illustration6,
-      },
-      {
-          id: 7,
-          title: 'Illustration 7',
-          imgSrc: Illustration7,
-      },
-      {
-          id:8,
-          title: 'Illustration 8',
-          imgSrc: Illustration8,
-      },
-      {
-          id: 9,
-          title: 'Illustration 9',
-          imgSrc: Illustration9,
-      },
-      {
-        id: 10,
-        title: 'Illustration 10',
-        imgSrc: Illustration10,
-      },
+      { id: 1, title: 'Illustration 1', imgSrc: Illustration1, },
+      { id: 2, title: 'Illustration 2', imgSrc: Illustration2, },
+      { id: 3, title: 'Illustration 3', imgSrc: Illustration3, },
+      { id: 4, title: 'Illustration 4', imgSrc: Illustration4, },
+      { id: 5, title: 'Illustration 5', imgSrc: Illustration5, },
+      { id: 6, title: 'Illustration 6', imgSrc: Illustration6, },
+      { id: 7, title: 'Illustration 7', imgSrc: Illustration7, },
+      { id: 8, title: 'Illustration 8', imgSrc: Illustration8, },
+      { id: 9, title: 'Illustration 9', imgSrc: Illustration9, },
+      { id: 10, title: 'Illustration 10', imgSrc: Illustration10, },
     ],
     storyboardMenuItems : [
-      {
-        title: 'Night Drive',
-        iconImage: NightDrive1,
-        path: '/NightDrive'
-      },
-      {
-        title: 'Soar',
-        iconImage: Soar1,
-        path: '/Soar'
-      }
+      { title: 'Night Drive', iconImage: NightDrive1, path: '/NightDrive', color: 'red' },
+      { title: 'Soar', iconImage: Soar1, path: '/Soar', color: 'green' },
     ],
     nightDriveBoard : [
-      {
-        id: 1,
-        title: 'Night Drive 1',
-        imgSrc: NightDrive1,
-      },
-      {
-        id: 2,
-        title: 'Night Drive 2',
-        imgSrc: NightDrive2,
-      },
-      {
-        id: 3, 
-        title: 'Night Drive 3',
-        imgSrc: NightDrive3,
-      },
-      {
-        id: 4,
-        title: 'Night Drive 4',
-        imgSrc: NightDrive4,
-      },
-      {
-        id: 5,
-        title: 'Night Drive 5',
-        imgSrc: NightDrive5,
-      },
-      {
-        id: 6,
-        title: 'Night Drive 6',
-        imgSrc: NightDrive6,
-      },
-      {
-        id: 7,
-        title: 'Night Drive 7',
-        imgSrc: NightDrive7,
-      },
-      {
-        id: 8,
-        title: 'Night Drive 8',
-        imgSrc: NightDrive8,
-      },
-      {
-        id: 9,
-        title: 'Night Drive 9',
-        imgSrc: NightDrive9,
-      },
-      {
-        id: 10,
-        title: 'Night Drive 10',
-        imgSrc: NightDrive10,
-      },
-      {
-        id: 11,
-        title: 'Night Drive 11',
-        imgSrc: NightDrive11,
-      },
-      {
-        id: 12,
-        title: 'Night Drive 12 ',
-        imgSrc: NightDrive12,
-      },
-      {
-        id: 13, 
-        title: 'Night Drive 13',
-        imgSrc: NightDrive13,
-      },
-      {
-        id: 14,
-        title: 'Night Drive 14',
-        imgSrc: NightDrive14,
-      },
-      {
-        id: 15,
-        title: 'Night Drive 15',
-        imgSrc: NightDrive15,
-      },
-      {
-        id: 16,
-        title: 'Night Drive 16',
-        imgSrc: NightDrive16,
-      },
-      {
-        id: 17,
-        title: 'Night Drive 17',
-        imgSrc: NightDrive17,
-      },
-      {
-        id: 18,
-        title: 'Night Drive 18',
-        imgSrc: NightDrive18,
-      },
-      {
-        id: 19,
-        title: 'Night Drive 19',
-        imgSrc: NightDrive19,
-      },
+      { id: 1, title: 'Night Drive 1', imgSrc: NightDrive1, },
+      { id: 2, title: 'Night Drive 2', imgSrc: NightDrive2, },
+      { id: 3, title: 'Night Drive 3', imgSrc: NightDrive3, },
+      { id: 4, title: 'Night Drive 4', imgSrc: NightDrive4, },
+      { id: 5, title: 'Night Drive 5', imgSrc: NightDrive5, },
+      { id: 6, title: 'Night Drive 6', imgSrc: NightDrive6, },
+      { id: 7, title: 'Night Drive 7', imgSrc: NightDrive7, },
+      { id: 8, title: 'Night Drive 8', imgSrc: NightDrive8, },
+      { id: 9, title: 'Night Drive 9', imgSrc: NightDrive9, },
+      { id: 10, title: 'Night Drive 10', imgSrc: NightDrive10, },
+      { id: 11, title: 'Night Drive 11', imgSrc: NightDrive11, },
+      { id: 12, title: 'Night Drive 12', imgSrc: NightDrive12, },
+      { id: 13, title: 'Night Drive 13', imgSrc: NightDrive13, },
+      { id: 14, title: 'Night Drive 14', imgSrc: NightDrive14, },
+      { id: 15, title: 'Night Drive 15', imgSrc: NightDrive15, },
+      { id: 16, title: 'Night Drive 16', imgSrc: NightDrive16, },
+      { id: 17, title: 'Night Drive 17', imgSrc: NightDrive17, },
+      { id: 18, title: 'Night Drive 18', imgSrc: NightDrive18, },
+      { id: 19, title: 'Night Drive 19', imgSrc: NightDrive19, },
     ],
     soarBoard : [
-      {
-        id: 1,
-        title: 'Soar 1',
-        imgSrc: Soar1,
-      },
-      {
-        id: 2,
-        title: 'Soar 2 ',
-        imgSrc: Soar2,
-      },
-      {
-        id: 3, 
-        title: 'Soar 3',
-        imgSrc: Soar3,
-      },
-      {
-        id: 4,
-        title: 'Soar 4',
-        imgSrc: Soar4,
-      },
-      {
-        id: 5,
-        title: 'Soar 5',
-        imgSrc: Soar5,
-      },
-      {
-        id: 6,
-        title: 'Soar 6',
-        imgSrc: Soar6,
-      },
-      {
-        id: 7,
-        title: 'Soar 7',
-        imgSrc: Soar7,
-      },
-      {
-        id: 8,
-        title: 'Soar 8',
-        imgSrc: Soar8,
-      },
-      {
-        id: 9,
-        title: 'Soar 9',
-        imgSrc: Soar9,
-      },
-      {
-        id: 10,
-        title: 'Soar 10',
-        imgSrc: Soar10,
-      },
-      {
-        id: 11,
-        title: 'Soar 11',
-        imgSrc: Soar11,
-      },
-      {
-        id: 12,
-        title: 'Soar 12 ',
-        imgSrc: Soar12,
-      },
-      {
-        id: 13, 
-        title: 'Soar 13',
-        imgSrc: Soar13,
-      },
-      {
-        id: 14,
-        title: 'Soar 14',
-        imgSrc: Soar14,
-      },
-      {
-        id: 15,
-        title: 'Soar 15',
-        imgSrc: Soar15,
-      },
-      ],
-    home: {
-      title: 'Tanner Jensen',
-      subTitle: 'Pictures and stuff',
-      text: 'Check em out'
-    },
-    InkIllustrationsPage: {
-      title: 'Tanner Jensen',
-      subTitle: 'Pictures and stuff',
-      text: 'Check em out'
-    },
-    ComicsPage: {
-      title: 'Tanner Jensen',
-      subTitle: 'Pictures and stuff',
-      text: 'Check em out'
-    },
-    StoryboardsPage: {
-      title: 'Tanner Jensen',
-      subTitle: 'Pictures and stuff',
-      text: 'Check em out'
-    },
-    ContactPage: {
-      title: 'Tanner Jensen',
-      subTitle: 'Pictures and stuff',
-      text: 'Check em out'
-    },
+      { id: 1, title: 'Soar 1', imgSrc: Soar1, },
+      { id: 2, title: 'Soar 2', imgSrc: Soar2, },
+      { id: 3, title: 'Soar 3', imgSrc: Soar3, },
+      { id: 4, title: 'Soar 4', imgSrc: Soar4, },
+      { id: 5, title: 'Soar 5', imgSrc: Soar5, },
+      { id: 6, title: 'Soar 6', imgSrc: Soar6, },
+      { id: 7, title: 'Soar 7', imgSrc: Soar7, },
+      { id: 8, title: 'Soar 8', imgSrc: Soar8, },
+      { id: 9, title: 'Soar 9', imgSrc: Soar9, },
+      { id: 10, title: 'Soar 10', imgSrc: Soar10, },
+      { id: 11, title: 'Soar 11', imgSrc: Soar11, },
+      { id: 12, title: 'Soar 12', imgSrc: Soar12, },
+      { id: 13, title: 'Soar 13', imgSrc: Soar13, },
+      { id: 14, title: 'Soar 14', imgSrc: Soar14, },
+      { id: 15, title: 'Soar 15', imgSrc: Soar15, },
+    ],
   }
 
   render() {
     return (
       <Router>
         <NavBar />
-            <switch>
-              <div className="flex-container">
-                <Route exact path='/Comics'>
-                  <Comics comics = {this.state.comics}/>
-                </Route>
-                <Route path='/InkIllustrations'>
-                  <InkIllustrations inkIllustrations = {this.state.inkIllustrations}/>
-                </Route>
-                <Route path='/Storyboards'>
-                  <StoryboardMenu storyboardMenuItems = {this.state.storyboardMenuItems}/>
-                </Route>
-                <Route path='/NightDrive'>
-                  <StoryboardCarousel  storyboards = {this.state.nightDriveBoard}/>
-                </Route>
-                <Route path='/Soar'>
-                  <StoryboardCarousel storyboards = {this.state.soarBoard}/>
-                </Route>
-                <Route path='/Contact'>
-                  <Contact />
-                </Route>
-              </div>
-            </switch>
+        <AppContainer>
+          <switch>
+            <Route exact path='/Comics'>
+              <Gallery images = {this.state.comics}/>
+            </Route>
+            <Route path='/InkIllustrations'>
+              <Gallery images = {this.state.inkIllustrations}/>
+            </Route>
+            <Route path='/Storyboards'>
+              <StoryboardMenu storyboardMenuItems = {this.state.storyboardMenuItems}/>
+            </Route>
+            <Route path='/NightDrive'>
+              <StoryboardCarousel  storyboards = {this.state.nightDriveBoard}/>
+            </Route>
+            <Route path='/Soar'>
+              <StoryboardCarousel storyboards = {this.state.soarBoard}/>
+            </Route>
+            <Route path='/Contact'>
+              <Contact />
+            </Route>
+          </switch>
+        </AppContainer>
       </Router>   
     );
   }
 }
 
 export default App;
+
+
